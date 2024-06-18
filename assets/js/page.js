e89aba21ed5a53a9b1ebe66df70cf62d7126e096
OUMARNDIAYE49 <oumar.ndiaye4952@gmail.com>
2024-06-18 00:51:21 +0000

    first commit

---
+++ assets/js/page.js
@@ -0,0 +1,24 @@
+// function showSidebar() {
+//     const sidebar = document.querySelector('.sidebar');
+//     sidebar.style.display = 'flex';
+// }
+
+// function hideSidebar() {
+//     const sidebar = document.querySelector('.sidebar');
+//     sidebar.style.display = 'none';
+// }
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
+
+// Add event listeners to all sections
+document.getElementById('contact').addEventListener('click', function() {
+    hideSidebar();
+    window.location.hash = '#contact';
+});
